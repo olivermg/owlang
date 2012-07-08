eca0cb6324a8bd66f6c539e720a13803fdb24659
Oliver Wegner <void1976@gmail.com>
2012-07-08 20:56:33 +0200

    added dump_module to main.ml

--- lexer_parser_ocaml/main.ml
+++ lexer_parser_ocaml/main.ml
@@ -9,7 +9,8 @@ let double_type = double_type context
 let main _ = let v1 = const_float double_type 3.0 in
 	let v2 = const_float double_type 4.0 in
 	let vadd = build_fadd v1 v2 "addtmp" builder in
-	dump_value vadd
+	dump_value vadd;
+	dump_module the_module
 
 let _ = Printexc.print main ()
 
