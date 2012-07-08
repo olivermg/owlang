79842f1de08b3e67b4522f9026a0968d0ccc0a10
Oliver Wegner <void1976@gmail.com>
2012-07-08 17:38:21 +0200

    added dummy llvm stuff to main.ml

--- lexer_parser_ocaml/main.ml
+++ lexer_parser_ocaml/main.ml
@@ -1,6 +1,15 @@
+open Printf
 open Llvm
 
-let main _ = global_context ()
+let context = global_context ()
+let the_module = create_module context "yeah cool jit"
+let builder = builder context
+let double_type = double_type context
+
+let main _ = let v1 = const_float double_type 3.0 in
+	let v2 = const_float double_type 4.0 in
+	let vadd = build_fadd v1 v2 "addtmp" builder in
+	dump_value vadd
 
 let _ = Printexc.print main ()
 
