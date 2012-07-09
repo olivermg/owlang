a53cc5ade98fdbeafd46e15f409a975e07708329
Oliver Wegner <void1976@gmail.com>
2012-07-09 21:28:46 +0200

    added some statements for setting value names

--- lexer_parser_ocaml/main.ml
+++ lexer_parser_ocaml/main.ml
@@ -9,6 +9,8 @@ let double_type = double_type context
 let main _ = let v1 = const_float double_type 3.0 in
 	let v2 = const_float double_type 4.0 in
 	let vadd = build_fadd v1 v2 "addtmp" builder in
+	set_value_name "var1" v1;
+	printf "name of value v1: %s\n" (value_name v1);
 	dump_value vadd;
 	dump_module the_module
 
